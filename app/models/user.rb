542f43a142e27f2807d116cdf2a5f38bb507db44
torako-san <gldemo1207@gmail.com>
2022-03-21 19:05:24 +0900

    トップページの作成

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :given_name, presence: true
+  validates :family_name_katakana, presence: true
+  validates :given_name_katakana, presence: true
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+end
