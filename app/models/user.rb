ee14a79f604859ddf82b930f4d9fad369696bfcc
torako-san <gldemo1207@gmail.com>
2022-03-22 08:40:38 +0900

    名前のカタカタバリテーションの記述を統合

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: "に全角文字を使用してください" } do
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: "に全角文字を入力して下さい" } do
     validates :family_name, presence: true
     validates :given_name, presence: true
   end
 
-  validates :family_name_katakana, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "はカタカナで入力して下さい。"}
-  validates :given_name_katakana, presence: true,format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "はカタカナで入力して下さい。"}
+  with_options presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "はカタカナで入力して下さい"} do
+    validates :family_name_katakana, presence: true
+    validates :given_name_katakana, presence: true
+  end
+
   validates :birthday, presence: true
-  
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英字と数字の両方を含めてください" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英字と数字の両方を含めて下さい" }
 
 end
