b66c67211a2e2f3ddd17042c9adcbcdfc10e8c8e
torako-san <gldemo1207@gmail.com>
2022-03-22 07:16:46 +0900

    バリテーションの定義直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :given_name, presence: true
-  validates :family_name_katakana, presence: true
-  validates :given_name_katakana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: "に全角文字を使用してください" } do
+    validates :family_name, presence: true
+    validates :given_name, presence: true
+  end
+
+  validates :family_name_katakana, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "はカタカナで入力して下さい。"}
+  validates :given_name_katakana, presence: true,format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "はカタカナで入力して下さい。"}
   validates :birthday, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英字と数字の両方を含めてください" }
 
 end
