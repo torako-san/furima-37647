a054a72c4761371f8bff05c99c3cb4f310fab744
torako-san <gldemo1207@gmail.com>
2022-03-22 17:05:49 +0900

    itemモデルのバリテーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
   belongs_to :delivery_burden
   belongs_to :delivery_area
   belongs_to :delivery_day
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_burden_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
