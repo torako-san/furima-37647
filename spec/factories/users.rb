4701122379887ec691d3cd7bec20b7cf693899ca
torako-san <gldemo1207@gmail.com>
2022-03-22 09:44:03 +0900

    factorybotのkey名が間違っていため修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname                { Faker::Name.initialas(nuber: 2) }
-    email                   { Faker::Internet.free_emeil }
+    nickname                { Faker::Name.initials(number: 2) }
+    email                   { Faker::Internet.free_email }
     password                { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation   { password }
     family_name             { person.first.kanji }
     given_name              { person.last.kanji }
-    family_name_kana        { person.first.katakana }
-    given_name_kana         { person.last.katakana }
-    date                    { Faker::Date.backward }
+    family_name_katakana    { person.first.katakana }
+    given_name_katakana     { person.last.katakana }
+    birthday                { Faker::Date.backward }
   end
 end
