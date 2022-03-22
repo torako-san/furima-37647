89c96b394bb61ee9aeb33a0dbf6cf3e9bd51d988
torako-san <gldemo1207@gmail.com>
2022-03-22 11:38:55 +0900

    passwordのバリテーションエラー時に出るメッセージ表示を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,6 @@ class User < ApplicationRecord
   end
 
   validates :birthday, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英字と数字の両方を含めて下さい" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は英字と数字の両方を含めて下さい" }
 
 end
