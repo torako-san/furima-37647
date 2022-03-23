4557d38af1dce0213a86b105c672558fc3220309
torako-san <gldemo1207@gmail.com>
2022-03-23 14:26:18 +0900

    itemモデルのFactoryBotを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,13 @@ FactoryBot.define do
   factory :item do
     name                { Faker::Name.initials(number: 5) }
     description         { Faker::Lorem.sentence }
-    price               { Faker::Number.between(from: 300, to: 9999999) }
+    price               { Faker::Number.between(from: 300, to: 9_999_999) }
+    item_category_id    { Faker::Number.between(from: 2, to: 12 ) }
+    item_status_id      { Faker::Number.between(from: 2, to: 7) }
+    delivery_burden_id  { Faker::Number.between(from: 2, to: 3) }
+    delivery_area_id    { Faker::Number.between(from: 2, to: 48) }
+    delivery_day_id     { Faker::Number.between(from: 2, to: 4) }
+    association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test.jpeg'), filename: 'test.jpeg')
