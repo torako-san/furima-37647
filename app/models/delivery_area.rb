80926cc2e658a18103ac073cfede37c551384561
torako-san <gldemo1207@gmail.com>
2022-03-23 16:02:42 +0900

    アクティブハッシュの未選択時の表示を修正

--- app/models/delivery_area.rb
+++ app/models/delivery_area.rb
@@ -1,6 +1,6 @@
 class DeliveryArea < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '---' },
     # 北海道・東北地方
     { id: 2, name: '北海道' },
     { id: 3, name: '青森県' },
--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,6 +1,6 @@
 class ItemCategory < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '---' },
     { id: 2, name: 'メンズ' },
     { id: 3, name: 'レディース' },
     { id: 4, name: 'ベビー・キッズ' },
