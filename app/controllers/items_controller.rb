cf2406b8de7b541b436abc27625ffcc01bf40b29
torako-san <gldemo1207@gmail.com>
2022-03-23 11:14:55 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image, :item_category_id, :item_status_id, :delivery_burden_id, :delivery_area_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
 end
