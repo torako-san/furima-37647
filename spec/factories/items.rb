53ff4cb59c5a0a78ea14a6d50c264851479b14ed
torako-san <gldemo1207@gmail.com>
2022-03-23 12:26:45 +0900

    FactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { Faker::Name.initials(number: 5) }
+    description         { Faker::Lorem.sentence }
+    price               { Faker::Number.between(from: 300, to: 9999999) }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.jpeg'), filename: 'test.jpeg')
+    end
   end
 end
