728c79f505a8f15aad23fd00e9a6ee6961bdade4
torako-san <gldemo1207@gmail.com>
2022-03-24 17:36:17 +0900

    Revert "destroyアクションの不要なインスタン変数への代入を削除"
    This reverts commit d3740b261381e758f9f5a5b7153aa4a634857b99.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @item = Item.order('created_at DESC')
@@ -26,8 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
