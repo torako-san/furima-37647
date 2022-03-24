64022304e61979f38fb7f74a50b5682412b712c7
torako-san <gldemo1207@gmail.com>
2022-03-24 17:05:13 +0900

    destoryアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find[params(:id)]
+    item.destroy
+    redirect_to root_path
+  end
+
   def update
     if @item.update(item_params)
       redirect_to item_path
