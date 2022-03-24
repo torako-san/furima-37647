b41747fed331b1666d20b2779a47f4051af58f39
torako-san <gldemo1207@gmail.com>
2022-03-24 16:36:43 +0900

    updateアクションのインスタン変数定義追加とメソッド化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @item = Item.order('created_at DESC')
@@ -26,8 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
