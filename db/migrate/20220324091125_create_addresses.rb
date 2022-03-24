86486497c5170a24516ef1543c39e140f38f6904
torako-san <gldemo1207@gmail.com>
2022-03-25 06:14:08 +0900

    addresssesテーブルのカラムを設定

--- db/migrate/20220324091125_create_addresses.rb
+++ db/migrate/20220324091125_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string        :post_code, null:false
+      t.integer       :delivery_area_id, null:false
+      t.string        :ctiy, null:false
+      t.string        :address_line, null:false
+      t.string        :address_building, null:false
+      t.string        :phone_number, null: false
+      t.references    :order, null: false, foreign_key: true
       t.timestamps
     end
   end
