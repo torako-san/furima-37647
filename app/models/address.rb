0deb287b352cdbf32ac48f8ffae9e1a59fd883f1
torako-san <gldemo1207@gmail.com>
2022-03-25 08:56:23 +0900

    addressモデルにバリテーションを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  validates :post_code, presence: true, format: { with: ^(?=./^[0-9]+$/)(/^\d{3}-\d{4}$/).*$ }
+  validates :city, presence: true
+  validates :address_line, presence: true
+  validates :address_building
+  validates :phone_number, presence: true, format: { with: ^(?=./^[0-9]+$/)(?=./^0\d{9,10}$/).*$ }
+
+  validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
 end
