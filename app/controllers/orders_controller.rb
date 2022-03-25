fbdc847cbdbaa1e755dccd8021d50fe394a46670
torako-san <gldemo1207@gmail.com>
2022-03-25 19:13:34 +0900

    購入時のアクションを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,15 @@
 class OrdersController < ApplicationController
   
   def index
-    @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.find(params[:id])
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      item_pay
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -19,13 +19,13 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_address).permit(:post_code, :delivery_area_id, :ctiy, :addresss_line, :phone_number).merge(user_id: current_user.id, item_id: @item.id )
+    params.require(:order_address).permit(:post_code, :delivery_area_id, :city, :address_line, :phone_number, :address_building ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
+      amount: @item.price,
       card: order_params[:token],
       currency:'jpy'
     )
