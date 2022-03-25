767edec3eecf62fabc6385c7ca4928fc27a6f735
torako-san <gldemo1207@gmail.com>
2022-03-25 12:00:12 +0900

    フォームモデルにaddressコントローラーの内容を記述したため、記述を削除

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,14 +1,2 @@
 class AddressesController < ApplicationController
-  def create
-    @address = Address.new(address_params)
-    if @address.save
-      redirect_to root_path
-    else
-      render :index
-    end
-  end
-
-  def address_params
-    params.require(:address).permit(:post_code, :delivery_area_id, :ctiy, :addresss_line, :phone_number).merge(order_id: params[:order.id])
-  end
 end
