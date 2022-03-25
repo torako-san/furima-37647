2f1826527802202617ebcc0c6e32992d2c6e53e9
torako-san <gldemo1207@gmail.com>
2022-03-26 06:35:35 +0900

    出品者が売り切れ商品の商品情報編集機能にパスからアクセスするとトップページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
+    unless @item.user_id == current_user.id && !@item.order.present? 
       redirect_to root_path
     end
   end
