a788d0d21539b1655825290afb51a33498f261b6
torako-san <gldemo1207@gmail.com>
2022-03-25 12:14:26 +0900

    決済処理記述を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      item_pay
       @order_address.save
       redirect_to root_path
     else
@@ -20,4 +21,13 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:post_code, :delivery_area_id, :ctiy, :addresss_line, :phone_number).merge(user_id: current_user.id, item_id: @item.id )
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
+    end
 end
