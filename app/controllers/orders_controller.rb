f2fd7337ea075091b418b3c0931e54638c3d78e0
torako-san <gldemo1207@gmail.com>
2022-03-25 11:19:10 +0900

    送られてきたフォームデータを保存する際の処理を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  
   def index
     @order = Order.new
-    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @order_address = OrderAddress.new
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:post_code, :delivery_area_id, :ctiy, :addresss_line, :phone_number).merge(user_id: current_user.id, item_id: @item.id )
   end
 end
