69882fe4fb30bde0b0e57c5e205cfcf97b5db96e
torako-san <gldemo1207@gmail.com>
2022-03-25 10:59:01 +0900

    addressモデルのバリテーションをorder_addressモデルに移動

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :city, :address_line, :address_building, :phone_number, :delivery_area_id, :item_id, :user_id
+
+  with_options presence: true do
+    validates :post_code, format: { with: ^/^\d{3}-\d{4}$/ }
+    validates :city
+    validates :address_line
+    validates :address_building
+    validates :phone_number, format: { with: /^0\d{9,10}$/ }
+  end
+
+  validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
 end
