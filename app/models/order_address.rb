2cdd6d2e452a906dbcc4ba01490e5e2c2cbee6b7
torako-san <gldemo1207@gmail.com>
2022-03-25 11:57:53 +0900

    ファームモデルのバリテーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,19 +3,19 @@ class OrderAddress
   attr_accessor :postal_code, :city, :address_line, :address_building, :phone_number, :delivery_area_id, :item_id, :user_id, :order_id
 
   with_options presence: true do
-    validates :post_code, format: { with: ^/^\d{3}-\d{4}$/ }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :address_line
     validates :address_building
-    validates :phone_number, format: { with: /^0\d{9,10}$/ }
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
   end
 
   validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  def.save
-    # 購入情報をを保存して、、変数に代入する
+  def save
+    # 購入情報をを保存して、、変数に代入
     order = Order.create(item_id: item_id, user_id: user_id)
     # 住所情報を保存して、order変数のidを指定
-    Address.create(post_code: post_code, ctiy: city, address_line: address_line, address_building: address_building, :phone_number: phone_number, delivery_area_id: id, order_id: order.id )
+    Address.create(post_code: post_code, ctiy: city, address_line: address_line, address_building: address_building, phone_number: phone_number, delivery_area_id: id, order_id: order.id )
   end
 end
