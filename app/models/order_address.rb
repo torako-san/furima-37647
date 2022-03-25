135a6663d487d5d39e641435d0c2abb6726ad870
torako-san <gldemo1207@gmail.com>
2022-03-25 11:06:23 +0900

    購入商品idと購入者idを変数に代入

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :city, :address_line, :address_building, :phone_number, :delivery_area_id, :item_id, :user_id
+  attr_accessor :postal_code, :city, :address_line, :address_building, :phone_number, :delivery_area_id, :item_id, :user_id, :order_id
 
   with_options presence: true do
     validates :post_code, format: { with: ^/^\d{3}-\d{4}$/ }
@@ -11,4 +11,9 @@ class OrderAddress
   end
 
   validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def.save
+    # 購入情報をを保存して、、変数に代入する
+    order = Order.create(item_id: item_id, user_id: user_id)
+  end
 end
